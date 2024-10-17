5023f14cf35faa1a779f0b121deb6095d880e99d
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2024-10-17 16:51:20 -0700

    fix nav menu in dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,7 +3,6 @@ import { ReactNode } from 'react';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { Bell, CircleUser, Home, LineChart, Menu, Package, Package2, Search, ShoppingCart, Users } from 'lucide-react';
-
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
@@ -15,8 +14,9 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { signOut } from 'next-auth/react';
 import { Input } from '@/components/ui/input';
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { Sheet, SheetContent, SheetTrigger, SheetClose } from '@/components/ui/sheet';
 
 const navigationItems = [
 	{ href: '/dashboard', icon: Home, label: 'Dashboard' },
@@ -24,6 +24,10 @@ const navigationItems = [
 	{ href: '/dashboard/b', icon: Package, label: 'Products' },
 	{ href: '/dashboard/c', icon: Users, label: 'Customers' },
 ];
+
+const handleLogout = () => {
+	signOut({ callbackUrl: '/' });
+};
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
 	const pathname = usePathname();
@@ -88,45 +92,30 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
 								<span className="sr-only">Toggle navigation menu</span>
 							</Button>
 						</SheetTrigger>
-						<SheetContent side="left" className="flex flex-col">
+						<SheetContent side="left" className="flex flex-col bg-white">
 							<nav className="grid gap-2 text-lg font-medium">
 								<Link href="#" className="flex items-center gap-2 text-lg font-semibold">
 									<Package2 className="h-6 w-6" />
 									<span className="sr-only">Acme Inc</span>
 								</Link>
-								<Link
-									href="#"
-									className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-									<Home className="h-5 w-5" />
-									Dashboard
-								</Link>
-								<Link
-									href="#"
-									className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground">
-									<ShoppingCart className="h-5 w-5" />
-									Orders
-									<Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-										6
-									</Badge>
-								</Link>
-								<Link
-									href="#"
-									className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-									<Package className="h-5 w-5" />
-									Products
-								</Link>
-								<Link
-									href="#"
-									className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-									<Users className="h-5 w-5" />
-									Customers
-								</Link>
-								<Link
-									href="#"
-									className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-									<LineChart className="h-5 w-5" />
-									Analytics
-								</Link>
+								{navigationItems.map((item) => (
+									<SheetClose asChild>
+										<Link
+											key={item.label}
+											href={item.href}
+											className={`mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground ${
+												pathname === item.href ? 'bg-muted text-primary' : 'text-muted-foreground'
+											}`}>
+											<item.icon className="h-5 w-5" />
+											{item.label}
+											{item.badge && (
+												<Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+													{item.badge}
+												</Badge>
+											)}
+										</Link>
+									</SheetClose>
+								))}
 							</nav>
 							<div className="mt-auto">
 								<Card>
@@ -145,52 +134,26 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
 							</div>
 						</SheetContent>
 					</Sheet>
-					<div className="w-full flex-1">
-						<form>
-							<div className="relative">
-								<Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-								<Input
-									type="search"
-									placeholder="Search products..."
-									className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-								/>
-							</div>
-						</form>
+					<div className="ml-auto">
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<Button variant="secondary" size="icon" className="rounded-full border-none">
+									<CircleUser className="h-5 w-5" />
+									<span className="sr-only">Toggle user menu</span>
+								</Button>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent align="end">
+								<DropdownMenuLabel>My Account</DropdownMenuLabel>
+								<DropdownMenuSeparator />
+								<DropdownMenuItem>Settings</DropdownMenuItem>
+								<DropdownMenuItem>Support</DropdownMenuItem>
+								<DropdownMenuSeparator />
+								<DropdownMenuItem onClick={handleLogout}>Logout</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
 					</div>
-					<DropdownMenu>
-						<DropdownMenuTrigger asChild>
-							<Button variant="secondary" size="icon" className="rounded-full">
-								<CircleUser className="h-5 w-5" />
-								<span className="sr-only">Toggle user menu</span>
-							</Button>
-						</DropdownMenuTrigger>
-						<DropdownMenuContent align="end">
-							<DropdownMenuLabel>My Account</DropdownMenuLabel>
-							<DropdownMenuSeparator />
-							<DropdownMenuItem>Settings</DropdownMenuItem>
-							<DropdownMenuItem>Support</DropdownMenuItem>
-							<DropdownMenuSeparator />
-							<DropdownMenuItem>Logout</DropdownMenuItem>
-						</DropdownMenuContent>
-					</DropdownMenu>
 				</header>
-				<main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-					{/* <div className="flex items-center">
-						<h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
-					</div>
-					<div
-						className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
-						x-chunk="dashboard-02-chunk-1">
-						<div className="flex flex-col items-center gap-1 text-center">
-							<h3 className="text-2xl font-bold tracking-tight">You have no products</h3>
-							<p className="text-sm text-muted-foreground">
-								You can start selling as soon as you add a product.
-							</p>
-							<Button className="mt-4">Add Product</Button>
-						</div>
-					</div> */}
-					{children}
-				</main>
+				<main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">{children}</main>
 			</div>
 		</div>
 	);
