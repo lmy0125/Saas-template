83203dfff321a59fb2549e23730e98478b7ff54b
lmy0125 <50187300+lmy0125@users.noreply.github.com>
2024-10-19 03:05:56 -0700

    applied PrismaAdapter

--- src/auth.ts
+++ src/auth.ts
@@ -4,7 +4,7 @@ import { PrismaAdapter } from '@auth/prisma-adapter';
 import prisma from '@/lib/prisma';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-	// adapter: PrismaAdapter(prisma),
+	adapter: PrismaAdapter(prisma),
 	providers: [Google],
 	callbacks: {
 		authorized: async ({ auth }) => {
